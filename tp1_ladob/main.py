3cf2fd15ae69a61056a367714086c0ceb07e59d8
sferraris <sferraris@itba.edu.ar>
2022-09-03 18:03:56 -0300

    Arreglo algoritmo, agrego mutacion
    Co-authored-by: Micaela Capart <mcapart@itba.edu.ar>
    Co-authored-by: Mateo Ruiz <matruiz@itba.edu.ar>

--- tp1_ladob/main.py
+++ tp1_ladob/main.py
@@ -10,18 +10,19 @@ class Color:
     def __init__(self, red, green, blue):
         self.rgb = [red, green, blue]
         self.fitness = 0
+        self.uses = 0
 
     def __str__(self):
-        return f"r:{self.rgb[0]},g:{self.rgb[1]},b:{self.rgb[2]}"
+        return f"r:{self.rgb[0]},g:{self.rgb[1]},b:{self.rgb[2]}, uses: {self.uses}, fitness: {self.fitness}"
 
     def __repr__(self):
-        return f"r:{self.rgb[0]},g:{self.rgb[1]},b:{self.rgb[2]}"
+        return f"r:{self.rgb[0]},g:{self.rgb[1]},b:{self.rgb[2]}, uses: {self.uses}, fitness: {self.fitness}"
 
     def __gt__(self, other):
-        return self.fitness.__gt__(other.fitness)
+        return self.fitness.__lt__(other.fitness)
 
     def __lt__(self, other):
-        return self.fitness.__lt__(other.fitness)
+        return self.fitness.__gt__(other.fitness)
 
     def __eq__(self, other):
         return self.rgb.__eq__(other.rgb)
@@ -38,12 +39,19 @@ def elite_selection_mode(color_array: [], k: int):
     # receives a sorted_by_fitness color array
     selected_colors = []
     n = len(color_array)
+    used_colors = 0
     for pos in range(len(color_array)):
         number_to_select = math.ceil((k - pos) / n)
-        if number_to_select > 0:
-            for p in range(number_to_select):
-                if len(selected_colors) < k:
-                    selected_colors.append(copy.deepcopy(color_array[pos]))
+        color = copy.deepcopy(color_array[pos])
+        if used_colors + number_to_select <= k:
+            color.uses = number_to_select
+            used_colors += color.uses
+            selected_colors.append(color)
+        else:
+            color.uses = k - used_colors
+            used_colors += color.uses
+            selected_colors.append(color)
+            return selected_colors
 
     return selected_colors
 
@@ -66,13 +74,13 @@ def crossover(c1: Color, c2: Color):
 
 
 def get_children(parent_array: []):
-    # [c1., c1, c2., c2, c3]
+    # [c1, c2, c3, c4, c5]
     # [[rgb1, rgb2]]
     # [c1, c2, c3]
-    #
+
     used = []
     children_colors = []
-    print(parent_array)
+    """
     for p1 in range(len(parent_array)):
         for p2 in range(len(parent_array)):
             first_parent = parent_array[p1]
@@ -82,15 +90,25 @@ def get_children(parent_array: []):
                     and not first_parent.__eq__(second_parent) \
                     and not used.__contains__([p1, p2]) \
                     and not used.__contains__([p2, p1]):
-                print("entra")
                 first_child, second_child = crossover(first_parent, second_parent)
                 children_colors.append(first_child)
                 children_colors.append(second_child)
                 used.append([p1, p2])
                 parent_array[p1] = None
-                parent_array[p2] = None
+                parent_array[p2] = None   
+    """
+    for p1 in range(len(parent_array)):
+        first_parent = parent_array[p1]
+        if first_parent.uses > 0:
+            for p2 in range(p1 + 1, len(parent_array)):
+                second_parent = parent_array[p2]
+                if first_parent.uses > 0 and second_parent.uses > 0:
+                    first_child, second_child = crossover(first_parent, second_parent)
+                    first_parent.uses -= 1
+                    second_parent.uses -= 1
+                    children_colors.append(first_child)
+                    children_colors.append(second_child)
 
-    print(used)
     return children_colors
 
 
@@ -100,6 +118,19 @@ def print_color_palette(colors: []):
         print(color)
 
 
+def mutation(color: Color, mutation_probability, mutation_delta):
+    color.rgb[0] += get_mutation_delta(mutation_probability, mutation_delta)
+    color.rgb[1] += get_mutation_delta(mutation_probability, mutation_delta)
+    color.rgb[2] += get_mutation_delta(mutation_probability, mutation_delta)
+
+
+def get_mutation_delta(mutation_probability, mutation_delta):
+    mutation_decider = random.randint(0, 100)
+    if mutation_decider < mutation_probability:
+        return random.randint(-mutation_delta, mutation_delta)
+    return 0
+
+
 def main():
     config_file = open("config.json")
     config_data = json.load(config_file)
@@ -107,6 +138,8 @@ def main():
     target_color_data = config_data["target_color"]
     population_length = config_data["population_length"]
     K = config_data["K"]
+    mutation_probability = config_data["mutation_probability"]
+    mutation_delta = config_data["mutation_delta"]
 
     target_color = Color(target_color_data[0], target_color_data[1], target_color_data[2])
     current_color_gen = []
@@ -120,24 +153,48 @@ def main():
         bisect.insort(current_color_gen, color)
 
     print_color_palette(current_color_gen)
-    max_fitness = 0
+
+    # START TEST
+    best_r = 1000
+    best_g = 1000
+    best_b = 1000
+
+    for color in current_color_gen:
+        if abs(color.rgb[0] - target_color.rgb[0]) < abs(best_r - target_color.rgb[0]):
+            best_r = color.rgb[0]
+        if abs(color.rgb[1] - target_color.rgb[1]) < abs(best_g - target_color.rgb[1]):
+            best_g = color.rgb[1]
+        if abs(color.rgb[2] - target_color.rgb[2]) < abs(best_b - target_color.rgb[2]):
+            best_b = color.rgb[2]
+
+    testcolor = Color(best_r, best_g, best_b)
+    calculate_fitness(target_color, testcolor)
+
+    print(
+        f"Test Color: {testcolor}, fitness: {testcolor.fitness}")
+    # END TEST
+
     # Para cada i, hay que hacer selection, crossover, mutation
-    for i in range(10):
+    for i in range(100):
+        # Parent selection
         parent_colors = elite_selection_mode(current_color_gen, K)
+        # Crossover
         children_colors = get_children(copy.deepcopy(parent_colors))
-        # TODO mutation ver que onda dsps, preguntarle a PAU
+        # Child mutation
+        for child in children_colors:
+            mutation(child, mutation_probability, mutation_delta)
+
         # Fill-All implementation, cuando vuelve a entrar hace un elite selection
         current_color_gen = []
         for color in copy.deepcopy(parent_colors) + copy.deepcopy(children_colors):
             calculate_fitness(target_color, color)
-            if color.fitness > max_fitness:
-                max_fitness = color.fitness
-            bisect.insort(current_color_gen, color)
+            if not current_color_gen.__contains__(color):
+                bisect.insort(current_color_gen, color)
 
     print_color_palette(current_color_gen)
 
     print(
-        f"Selected Color: {current_color_gen[0]}, fitness: {current_color_gen[0].fitness}, max_fitness: {max_fitness}")
+        f"Selected Color: {current_color_gen[0]}, fitness: {current_color_gen[0].fitness}")
 
 
 if __name__ == "__main__":
