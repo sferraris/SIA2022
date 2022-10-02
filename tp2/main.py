1561baecca62d798e6c011a5068f42c8d9a95ae7
Mica <mcapart@itba.edu.ar>
2022-10-02 00:20:00 -0300

    Arreglos generales

--- tp2/main.py
+++ tp2/main.py
@@ -18,13 +18,15 @@ def perceptron_run(points: [], n: float, cot: int, error: float, dim: int, perce
     w = []
     for j in range(dim + 1):
         w.append(0.0)
+    w = numpy.random.uniform(-1, 1, dim+1)
+    print(w)
     error_min = math.inf
     perceptron: declarations.Perceptron = declarations.Perceptron(n, w)
-
+    w_min = []
     while error_min > 0 and i < cot:
+        m = random.randint(0, len(points) - 1)
+        h = calculate_excitement(points[m], perceptron.w)
         if perceptron_type == 'step':
-            m = random.randint(0, len(points) - 1)
-            h = calculate_excitement(points[m], perceptron.w)
             o = numpy.sign(h)
             if o == 0:
                 o = 1
@@ -32,20 +34,21 @@ def perceptron_run(points: [], n: float, cot: int, error: float, dim: int, perce
             new_w: [] = calculate_w(delta_w, perceptron.w)
             error = calculate_error_step(points, new_w)
         elif perceptron_type == 'linear':
-            delta_w = calculate_delta_w_linear(points, perceptron.n, dim, perceptron.w)
+            o = h
+            delta_w = calculate_delta_w_step(points[m], perceptron.n, o)
             new_w: [] = calculate_w(delta_w, perceptron.w)
-            error = calculate_error_step(points, new_w)
+            error = calculate_error_linear(points, new_w)
         elif perceptron_type == 'non-linear-tan':
-            delta_w = calculate_delta_w_non_linear_tan(points, perceptron.n, dim, perceptron.w, b)
+            o = math.tanh(b * h)
+            delta_w = calculate_delta_w_non_linear_tan(points[m], perceptron.n, o, b)
             new_w: [] = calculate_w(delta_w, perceptron.w)
-            error = calculate_error_step(points, new_w)
+            error = calculate_error_linear(points, new_w)
         elif perceptron_type == 'non-linear-logistic':
-            delta_w = calculate_delta_w_non_linear_logistic(points, perceptron.n, dim, perceptron.w, b)
+            o = 1 / (1 + math.e ** (-2 * b * h))
+            delta_w = calculate_delta_w_non_linear_logistic(points[m], perceptron.n, o, b)
             new_w: [] = calculate_w(delta_w, perceptron.w)
-            error = calculate_error_step(points, new_w)
+            error = calculate_error_linear(points, new_w)
         else:
-            m = random.randint(0, len(points) - 1)
-            h = calculate_excitement(points[m], perceptron.w)
             o = numpy.sign(h)
             if o == 0:
                 o = 1
@@ -53,11 +56,12 @@ def perceptron_run(points: [], n: float, cot: int, error: float, dim: int, perce
             new_w: [] = calculate_w(delta_w, perceptron.w)
             error = calculate_error_step(points, new_w)
 
+        perceptron.w = new_w
         if error <= error_min:
             error_min = error
-            perceptron.w = new_w
+            w_min = new_w
         i += 1
-
+    perceptron.w = w_min
     print(f"i: {i}")
     print(f"error: {error_min}")
     return perceptron
@@ -71,7 +75,7 @@ def calculate_excitement(point: declarations.Point, w: []):
     return excitement
 
 
-def calculate_delta_w_step(point: declarations.Point, n: float, o: int):
+def calculate_delta_w_step(point: declarations.Point, n: float, o: float):
     delta_w = []
     wi = n * (point.expected_value - o)
     for ei in point.e:
@@ -94,32 +98,44 @@ def calculate_delta_w_linear(points: [], n: float, dim: int, w: []):
     return delta_w
 
 
-def calculate_delta_w_non_linear_tan(points: [], n: float, dim: int, w: [], b: float):
+def calculate_delta_w_non_linear_tan(point: declarations.Point, n: float, o: float, b: float):
+    #delta_w = []
+    #for j in range(dim + 1):
+    #    delta_w.append(0.0)
+    #
+    #for point in points:
+    #    h = calculate_excitement(point, w)
+    #    o = math.tanh(b * h)
+    #    wi = n * (point.expected_value - o) * b * (1 - o)
+    #    for i in range(len(point.e)):
+    #        delta_w[i] += wi * point.e[i]
+    #
+    #return delta_w
     delta_w = []
-    for j in range(dim + 1):
-        delta_w.append(0.0)
-
-    for point in points:
-        h = calculate_excitement(point, w)
-        o = math.tanh(b * h)
-        wi = n * (point.expected_value - o) * b * (1 - o)
-        for i in range(len(point.e)):
-            delta_w[i] += wi * point.e[i]
+    wi = n * (point.expected_value - o) * b * (1 - o)
+    for ei in point.e:
+        delta_w.append(wi * ei)
 
     return delta_w
 
 
-def calculate_delta_w_non_linear_logistic(points: [], n: float, dim: int, w: [], b: float):
+def calculate_delta_w_non_linear_logistic(point: declarations.Point, n: float, o: float, b: float):
+    #delta_w = []
+    #for j in range(dim + 1):
+    #    delta_w.append(0.0)
+    #
+    #for point in points:
+    #    h = calculate_excitement(point, w)
+    #    o = 1 / (1 + math.e ** (-2*b*h))
+    #    wi = n * (point.expected_value - o) * 2 * b * o * (1 - o)
+    #    for i in range(len(point.e)):
+    #        delta_w[i] += wi * point.e[i]
+    #
+    #return delta_w
     delta_w = []
-    for j in range(dim + 1):
-        delta_w.append(0.0)
-
-    for point in points:
-        h = calculate_excitement(point, w)
-        o = 1 / (1 + math.e ** (-2*b*h))
-        wi = n * (point.expected_value - o) * 2 * b * o * (1 - o)
-        for i in range(len(point.e)):
-            delta_w[i] += wi * point.e[i]
+    wi = n * (point.expected_value - o) * 2 * b * o * (1 - o)
+    for ei in point.e:
+        delta_w.append(wi * ei)
 
     return delta_w
 
@@ -144,6 +160,7 @@ def calculate_error_linear(points: [], w: []):
     error = 0
     for point in points:
         h = calculate_excitement(point, w)
+        #print(f"h: {h}, point: {point}")
         error += math.pow(point.expected_value - h, 2)
 
     return error * 1 / 2
@@ -162,6 +179,8 @@ def main():
 
     perceptron_type = config_data["perceptron_type"]
     point_array = []
+    training_set = []
+    evaluation_set = []
     dim = 0
     if perceptron_type == 'step':
         for i in range(len(x)):
@@ -171,6 +190,8 @@ def main():
             point = declarations.Point(arr, y[i])
             point_array.append(point)
             dim = len(x[0])
+        training_set = point_array
+        evaluation_set = point_array
     elif perceptron_type == 'linear' or perceptron_type == 'non-linear-tan' or perceptron_type == 'non-linear-logistic':
         file = open('TP2-ej2-conjunto.csv')
         csvreader = csv.reader(file)
@@ -186,12 +207,19 @@ def main():
                     arr.append(float(row[i]))
             point = declarations.Point(arr, expected_value)
             point_array.append(point)
+        for i in range(len(point_array)):
+            if i < len(point_array)/2:
+                training_set.append(point_array[i])
+            else:
+                evaluation_set.append(point_array[i])
+
     print(point_array)
 
-    perception = perceptron_run(point_array, n, cot, error, dim, perceptron_type, b)
+    perception = perceptron_run(training_set, n, cot, error, dim, perceptron_type, b)
 
     print(perception)
-
+    error = calculate_error_linear(evaluation_set, perception.w)
+    print(error)
     if perceptron_type == 'step':
         print(f"y = {perception.w[1] / (-1 * perception.w[2])} * x + {perception.w[0] / (-1 * perception.w[2])}")
 
